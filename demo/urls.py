661930c351ed46bd290e86cbbf6525b197ea0c90
titusdishon <titusdishon@gmail.com>
2019-06-29 11:18:42 +0300

    outgoing request intergration

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PurchaseView.as_view()),
+    path("payment/", views.make_payment)
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
-# Create your views here.
+from django import forms
+from django.shortcuts import render
 
+
+class PaymentForm(forms.Form):
+   tid = forms.CharField(max_length=20)
+   reference = forms.CharField(max_length=150)
+   amount = forms.FloatField()
+
+def make_payment(request):
+ if request.method == 'POST':
+   payment_form = PaymentForm(request.POST)
+   if payment_form.is_valid():
+     tid = payment_form.cleaned_data.get("tid")
+     reference = payment_form.cleaned_data.get("reference")
+     amount = payment_form.cleaned_data.get("amount")
+     print("Amount : ", amount)
+     print("tid : ", tid)
+     print("reference : ", reference)
+     return render(request, "home.html", {"payment_information": "jsjsj"})
+
+ return render(request, "home.html", {"payment_information": "jsjsj"})
 
 class PurchaseView(TemplateView):
     template_name ="home.html"
+
